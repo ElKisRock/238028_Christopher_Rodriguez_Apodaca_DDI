3d95ed1e09c63ad507a2ea2dc0d8e335591ac13b
Chris <chr1st0ph3r5315@gmail.com>
2026-02-13 11:59:00 -0700

    [Elimina] Eliminar los archivos duplicados

--- Parcial 1/Repaso 1/js/script.js
+++
@@ -1,7 +0,0 @@
-//declaramos una variable y asignamos la referencia a nuestra etiqueta con el id cuadro
-document.body.style.background = 'darkblue';
-let cuadro = document.querySelector("#cuadro");
-
-cuadro.style.background = 'blue';
-cuadro.style.width = '400px';
-console.log(cuadro);
